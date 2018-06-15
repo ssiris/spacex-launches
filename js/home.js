34ecbfffc86fa35c95989cb5d4f19aa31f8900e9
siri skogsrud funnemark <sirifunnemark@gmail.com>
2018-06-15 11:38:13 +0200

    updated js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+
+//UPCOMING launches
 var getUpcomingLaunches = function () {
     var request = new XMLHttpRequest();
 
@@ -32,4 +34,53 @@ var getUpcomingLaunches = function () {
     }
 }
 
+
+//PREVIOUS launches
+
+var getPreviousLaunches = function () {
+    var request = new XMLHttpRequest();
+
+    // Opening a new connecting using GET to request data from API endpoint.
+    request.open('GET', 'https://api.spacexdata.com/v2/launches/', true);
+
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+        if (request.status >= 200 && request.status < 400) {
+        getLaunches(data);
+        }
+        else {
+            console.log('error');
+        }
+    };
+
+    // Send request
+    request.send();
+
+    // EGEN FUNKSJON!!!
+    var getLaunches = function (data) {
+        let list = document.getElementById('previous-launch-list');
+        var counter = 0;
+
+            console.log(data);
+
+        data.forEach(launch => {
+          if(counter <= 2) {
+            var date = new Date(launch.launch_date_utc);
+            list.innerHTML = list.innerHTML + '<li><a href="launch.html?id=' + launch.flight_number + '"><span>' + launch.mission_name + '</span><span>' + launch.rocket.rocket_name + '</span><span>' + date + '</span></a></li>';
+          }
+          counter++;
+        });
+
+    }
+}
+
+
+
+
+
+
+
+
+
 getUpcomingLaunches();
+getPreviousLaunches();
