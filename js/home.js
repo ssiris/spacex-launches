f7239174309a80396147c774df8b672608e15c84
siri skogsrud funnemark <sirifunnemark@gmail.com>
2018-06-17 08:59:55 +0200

    updated rockets

--- js/home.js
+++ js/home.js
@@ -47,6 +47,7 @@ var getPreviousLaunches = function () {
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
         getLaunches(data);
+        getRocketStats(data);
         }
         else {
             console.log('error');
@@ -70,15 +71,66 @@ var getPreviousLaunches = function () {
         });
 
     }
+
+    var getRocketStats = function(data) {
+      let falconNines = [];
+      let falconHeavy = [];
+      let falconOnes = [];
+      data.forEach(launch => {
+        if(launch.rocket.rocket_id === "falcon1") {
+          falconOnes.push(launch);
+        }
+        else if (launch.rocket.rocket_id === "falcon9") {
+          falconNines.push(launch);
+        }
+        else if (launch.rocket.rocket_id === "falconheavy") {
+          falconHeavy.push(launch);
+        }
+      });
+
+      var oneFirst = falconOnes[0];
+      var oneTotal = falconOnes.length;
+      var oneLast = falconOnes[oneTotal-1];
+      var oneFirstElement = document.getElementById('oneFirst');
+      var oneLastElement = document.getElementById('oneLast');
+      var oneTotalElement = document.getElementById('oneTotal');
+      var oneFirstDate = new Date(oneFirst.launch_date_utc);
+      var oneLastDate = new Date(oneLast.launch_date_utc);
+
+      oneFirstElement.innerHTML = '<a href="launch.html?id=' + oneFirst.flight_number + '">' + oneFirstDate.toLocaleDateString() + '</a>';
+      oneLastElement.innerHTML = '<a href="launch.html?id=' + oneLast.flight_number + '">' + oneLastDate.toLocaleDateString() + '</a>';
+      oneTotalElement.innerHTML = oneTotal;
+
+
+      var nineFirst = falconNines[0];
+      var nineTotal = falconNines.length;
+      var nineLast = falconNines[nineTotal-1];
+      var nineFirstElement = document.getElementById('nineFirst');
+      var nineLastElement = document.getElementById('nineLast');
+      var nineTotalElement = document.getElementById('nineTotal');
+      var nineFirstDate = new Date(nineFirst.launch_date_utc);
+      var nineLastDate = new Date(nineLast.launch_date_utc);
+
+      nineFirstElement.innerHTML = '<a href="launch.html?id=' + nineFirst.flight_number + '">' + nineFirstDate.toLocaleDateString() + '</a>';
+      nineLastElement.innerHTML = '<a href="launch.html?id=' + nineLast.flight_number + '">' + nineLastDate.toLocaleDateString() + '</a>';
+      nineTotalElement.innerHTML = nineTotal;
+
+
+      var heavyFirst = falconHeavy[0];
+      var heavyTotal = falconHeavy.length;
+      var heavyLast = falconHeavy[heavyTotal-1];
+      var heavyFirstElement = document.getElementById('heavyFirst');
+      var heavyLastElement = document.getElementById('heavyLast');
+      var heavyTotalElement = document.getElementById('heavyTotal');
+      var heavyFirstDate = new Date(heavyFirst.launch_date_utc);
+      var heavyLastDate = new Date(heavyLast.launch_date_utc);
+
+      heavyFirstElement.innerHTML = '<a href="launch.html?id=' + heavyFirst.flight_number + '">' + heavyFirstDate.toLocaleDateString() + '</a>';
+      heavyLastElement.innerHTML = '<a href="launch.html?id=' + heavyLast.flight_number + '">' + heavyLastDate.toLocaleDateString() + '</a>';
+      heavyTotalElement.innerHTML = heavyTotal;
+
+    }
 }
-
-
-
-
-
-
-
-
 
 getUpcomingLaunches();
 getPreviousLaunches();
